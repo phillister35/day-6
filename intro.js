d2c4d32a00e2bd7a6d34e3dbb698a85cb06a8a26
phillister35 <philippe.truong.dang@gmail.com>
2026-02-26 20:15:07 +0700

    day 6 exercises

---
+++ intro.js
@@ -0,0 +1,8 @@
+function introduction(name, age, address) {
+ 
+    console.log(`Name: ${name}`);
+    console.log(`Age: ${age}`);
+    console.log(`Address: ${address}`);
+}
+
+introduction('Truong Dang Gia Tue', 18, 'Saigon');
---
+++ name.js
@@ -0,0 +1,5 @@
+function printName(name) {
+    console.log(`What's up ${name}?`);
+}
+
+printName('Phil');
